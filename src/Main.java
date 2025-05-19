9e4bccfe1d02a2e469845635c2fd9d54d1d2a881
Алексей <erg163@ya.ru>
2025-05-19 10:04:24 +0400

    Ver 1.1

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,18 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+    public static void main(String[] args) {
+        int balance = 100;  // начальный баланс
+        int coming = 1001;  // сумма пополнения
+        int bonus = coming / 100;  // расчет бонусов
+        int x = coming + bonus + balance;  // баланс с бонусами
+        int x1 = coming + balance; // баланс без бонусов
+
+        if (coming > 1000) {
+            System.out.println("Бонус = " + bonus + "р.," + "баланс = " + x + "р.");
+        } else {
+            System.out.println("баланс = " + x1);
+
+        }
+
+
+    }
 }
